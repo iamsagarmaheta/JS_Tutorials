f689dfa1181717e51e47c5be74c008c5b28068d8
Maheta Sagar <iamsagarmaheta@gmail.com>
2023-10-06 20:42:14 +0530

    Added Array

--- index.js
+++ index.js
@@ -1,58 +1,90 @@
-// creating array
-let number_list = [11, 22, 33, 44, 55];
-let student_name = ['Sagar', 'Bob', 'Ronaldo', 66, 77, true, false, 3.14];
+let mod = 20 % 3;
+mod = -20.25 % 3;
+console.log(mod);
 
-let old_data = new Array('Pele', 'Maldini', 'Messi', 55, 7);
+let a = 10;
+let b = a++;   //    a++  post increment      ++a    pre increment
 
-let empty_data = [];
-let empty_list = new Array();
+console.log("B = " + b);
+console.log("A = " + a);
 
+a = 10;
+b = a--;   //    a--  post decrement      --a    pre decrement
 
-// accessing array
-console.log("3rd Element " + number_list[2]);
-console.log("Player " + old_data[2]);
+console.log("B = " + b);
+console.log("A = " + a);
 
-// updating array
-old_data[2] = "Ronaldo";
-console.log("Player " + old_data[2]);
-
-// size of array
-console.log("Size Of Array " + number_list.length);
-console.log(typeof number_list);    
-
-// add new element
-number_list[number_list.length] = 66;
-number_list.push(77);
-number_list.unshift(88); // enter value at beginning
-
-// remove element
-let deleted_number = number_list.pop();
-console.log( "deleted_number : " + deleted_number);
-
-// find index of elemetn
-let idx = old_data.indexOf('Pele');
-console.log( "idx : " + idx);
-
-idx = number_list.indexOf(666);
-console.log( "idx of 666 : " + idx);
+a = -a;
+a = -a;
+console.log("A = " + a);
 
 
-for(let index = 0; index < number_list.length; index++)
+console.log('10' == 10); // true
+console.log('10' === 10); // false
+
+console.log('10' != 10); // false
+console.log('10' !== 10); // true
+
+
+let stuname = "sagar";
+let bio = "";
+
+// if(bio == undefined)
+// {
+//     bio = stuname;
+// }
+
+//bio = bio || stuname;
+bio ||= stuname;
+console.log(bio);
+
+
+// power operator (Exponentiation Operator)
+let power = 8 ** 3;
+console.log(power);
+
+
+let age = 18;
+// 18+  you can vote
+// you can not vote
+if(age >= 18)
 {
-    //console.log(number_list[index]);
+    console.log("You Can Vote");
+}
+else
+{
+    console.log("You Can Not Vote")
+}
+
+console.log(age >= 18 ? "You Can Vote" : "You Can Not Vote");
+
+
+/*
+
+1
+12
+123
+1234
+12345
+
+*/
+let row = 1;
+let col = 1;
+while(row <= 5)
+{
+    col = 1;
+    let output = "";
+    while(col <= row)
+    {    
+        output = output + " " + col;
+        col++;
+    }
+    console.log(output);
+    row++;
 }
 
 
-// check if array
-if(Array.isArray(number_list) == true)
-{
-    console.log("number_list Is Array.");
-}
+let x = 10;
+let y = (x++, x + 1);
 
-let person = {
-    name: "sagar"
-};
-if(Array.isArray(person) == false)
-{
-    console.log("person Is No Array.");
-}
+console.log(x, y); //x = 11 , y = 12;
